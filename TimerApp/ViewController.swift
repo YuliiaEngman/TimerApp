a82866cdf1962464b01861e74c0e4dd3462c96d2
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-20 14:45:50 -0500

    Initial Commit

---
+++ TimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Yuliia Engman on 2/20/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
